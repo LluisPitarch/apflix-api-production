bdb71b4e3546cdcb0e7dbe929db93f656375a476
Lluis Pitarch <lluispitarchripolles@gmail.com>
2020-08-23 20:36:51 +0200

    remove build command

--- config/index.js
+++ config/index.js
@@ -2,14 +2,14 @@ require('dotenv').config();
 
 const config = {
     dev: process.env.NODE_ENV !== 'production',
-    port: process.env.PORT || 3000,
+    port: process.env.PORT,
     cors: process.env.CORS,
     db_user: process.env.DB_USER,
     db_password: process.env.DB_PASSWORD,
     db_host: process.env.DB_HOST,
     db_name: process.env.DB_NAME,
     defaultUserPassword: process.env.DEFAULT_USER_PASSWORD,
-    authJwtSecret: process.env.AUTH_JWT_SECRET || '',
+    authJwtSecret: process.env.AUTH_JWT_SECRET,
     publicApiKeyToken: process.env.PUBLIC_API_KEY_TOKEN,
     adminApiKeyToken: process.env.ADMIN_API_KEY_TOKEN,
     defaultAdminPassword: process.env.DEFAULT_ADMIN_PASSWORD,
